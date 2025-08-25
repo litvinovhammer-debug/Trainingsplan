b53232327ae98ef0af89b6ae170c94b423cbcd49
litvinovhammer-debug <litvinovhammer@gmail.com>
2025-08-25 03:41:47 +0200

    sw.js aktualisieren

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
   // Service Worker für Offline / Cache
-const CACHE = 'trainer-pwa-v21';   // <— Version anheben bei jedem Update!
+const CACHE = 'trainer-pwa-v22';   // <— Version anheben bei jedem Update!
 const FILES = [
   './',
   './index.html',
